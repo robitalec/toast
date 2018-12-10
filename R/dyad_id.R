397bb55669ead74e89428db14478ca373156757c
Alec Robitaille <robit.alec@gmail.com>
2018-12-10 12:42:37 -0800

    add both arg

--- R/dyad_id.R
+++ R/dyad_id.R
@@ -8,6 +8,7 @@
 #' @param DT data.table.
 #' @param focal focal individual.
 #' @param neighbour neighbours, optional - see Details.
+#' @param both if both is TRUE, returns duplicated dyadIDs with all focal/neighbour inverted and appended.
 #'
 #' @return
 #'
@@ -25,9 +26,12 @@
 #' DT <- fread(system.file("extdata", "DT.csv", package = "toast"))
 #'
 #' dyadDict <- dyad_id(DT, focal = 'ID', neighbour = NULL)
-dyad_id <- function(DT = NULL, focal = 'id', neighbour= NULL) {
+dyad_id <- function(DT = NULL,
+										focal = 'id',
+										neighbour = NULL,
+										both = FALSE) {
 	# NSE errors
-	. <- NULL
+	. <- dyadID <- NULL
 
 	check_col(DT, focal, 'focal')
 	check_type(DT, focal, c('numeric', 'character'))
@@ -36,48 +40,52 @@ dyad_id <- function(DT = NULL, focal = 'id', neighbour= NULL) {
 		ids <- DT[, data.table::CJ(unique(.SD[[1]]), unique(.SD[[1]])),
 							.SDcols = focal]
 
-		g <- igraph::graph_from_edgelist(
-			as.matrix(ids),
-			directed = FALSE
-		)
+		g <- igraph::graph_from_edgelist(as.matrix(ids),
+																		 directed = FALSE)
 
 		simpler <- igraph::simplify(g)
-		out <- data.table::data.table(
-			igraph::get.edgelist(simpler),
-			as.numeric(igraph::E(simpler)))
+		out <- data.table::data.table(igraph::get.edgelist(simpler),
+																	as.numeric(igraph::E(simpler)))
 
 		nms <- c('focal', 'neighbour', 'dyadID')
 		data.table::setnames(out, nms)
 
-		return(out)
-	}
 
-	if (!is.null(neighbour)) {
+
+		if (both) {
+			return(rbindlist(list(out,
+														out[, .(focal = neighbour,
+																		neighbour = focal,
+																		dyadID)])))
+		} else {
+			return(out)
+		}
+	} else if (!is.null(neighbour)) {
 		check_col(DT, neighbour, 'neighbour')
 		check_type(DT, neighbour, c('numeric', 'character'))
 
 		stop('in dev')
-	# if ('dyadID' %in% colnames(DT)) {
-	# 	warning('dropping dyadID from DT')
-	# 	DT[, dyadID := NULL]
-	# }
+		# if ('dyadID' %in% colnames(DT)) {
+		# 	warning('dropping dyadID from DT')
+		# 	DT[, dyadID := NULL]
+		# }
 
-	# # Double merge
-	# # First where left goes to dyadID
-	# # Then where right goes to dyadID2 and the order revered in by.y
-	# dyads <- merge(
-	# 	merge(DT,
-	# 				edgeDT,
-	# 				by.x = c(id, neighbour),
-	# 				by.y = edgeNames[1:2],
-	# 				all.x = TRUE),
-	# 	edgeDT[, .(id1, id2, dyadID2 = dyadID)],
-	# 	by.x = c(id, neighbour),
-	# 	by.y = edgeNames[2:1],
-	# 	all.x = TRUE)
-	#
-	# # Then dyadID filled with dyadID2
-	# # and dyadID2 dropped
-	# dyads[is.na(dyadID), dyadID := dyadID2][, dyadID2 := NULL][]
+		# # Double merge
+		# # First where left goes to dyadID
+		# # Then where right goes to dyadID2 and the order revered in by.y
+		# dyads <- merge(
+		# 	merge(DT,
+		# 				edgeDT,
+		# 				by.x = c(id, neighbour),
+		# 				by.y = edgeNames[1:2],
+		# 				all.x = TRUE),
+		# 	edgeDT[, .(id1, id2, dyadID2 = dyadID)],
+		# 	by.x = c(id, neighbour),
+		# 	by.y = edgeNames[2:1],
+		# 	all.x = TRUE)
+		#
+		# # Then dyadID filled with dyadID2
+		# # and dyadID2 dropped
+		# dyads[is.na(dyadID), dyadID := dyadID2][, dyadID2 := NULL][]
 	}
 }
