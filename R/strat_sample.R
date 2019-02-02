c1c1247b0f1a6980568ed0b570cf04317ba98ab4
Alec Robitaille <robit.alec@gmail.com>
2019-02-01 16:53:08 -0800

    man, specifically random

--- R/strat_sample.R
+++ R/strat_sample.R
@@ -2,10 +2,10 @@
 #'
 #' For each unique value in 'col', sample 'n' points and optionally return as a \code{data.table}.
 #'
-#' @param x
-#' @param n
-#' @param col
-#' @param returnDT
+#' @param x polygon object of class sf
+#' @param n number of random points
+#' @param col column in x indicating strata
+#' @param returnDT return a data.table (TRUE) or sf (FALSE) object
 #'
 #' @return Either a \code{sf} object or a \code{data.table} with a \code{sfc} (simple feature column).
 #' @export
@@ -14,7 +14,8 @@
 strat_sample <- function(x, n, col, returnDT = TRUE) {
 	lvls <- unique(x[[col]])
 	DT <- lapply(lvls, function(l) {
-		sf::st_sf(col = col, geometry = sf::st_sample(x[x[[col]] == l, ], n))
+		sf::st_sf(col = x[[col]],
+							geometry = sf::st_sample(x[x[[col]] == l, ], n, type = 'random'))
 	})
 
 	if(returnDT) {
